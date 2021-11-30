ddca4cb94aeca1949fcc545155c49a9159a77b38
Corentine <renuart.corentine@gmail.com>
2021-11-30 12:29:25 +0100

    Complete the Experiences page

--- src/elements/Knowledges/Experiences.js
+++ src/elements/Knowledges/Experiences.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Experiences = () => {
     return (
-        <div>
-            <h1>Expériences</h1>
+        <div className="Experiences">
+            <h3>Expériences</h3>
+            <div className="experience-1">
+                <h4>Junior front end developer</h4>
+                <h5>Juin 2021 - Janvier 2022</h5>
+                <p>7 mois de formation intensive afin d'apprendre le développement front-end et back-end.</p>
+            </div>
+            <div className="experience-2">
+                <h4>Strategie Agency</h4>
+                <h5>Novembre 2019 - Janvier 2020</h5>
+                <p>Stage de 3 mois m'ayant permis de créer des emballages et objets publicitaires pour différentes marques telles qu'ING, Léonidas, Galler, Lyreco et Tilman.</p>
+            </div>
         </div>
     );
 };
