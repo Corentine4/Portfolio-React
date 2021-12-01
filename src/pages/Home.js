f16f291c4b474d2364128dc0c50810eddc128358
Corentine <renuart.corentine@gmail.com>
2021-12-02 00:52:08 +0100

    Updates settings and html + css home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,16 @@ const Home = () => {
             {/* Import the navigation */}
             <Navigation/>
             {/* Main page */}
-            <div className="homeContent">
+            <div className="homeContainer">
                 <div className="homeContent">
-                    <h1>Corentine Renuart</h1>
-                    <h2>Développeuse Front-end</h2>
-                    <div className="pdfcv">
-                        <a href="./images/CV-Corentine-Renuart.pdf">Télécharger le curriculum vitae</a>
+                    <h1>Bienvenue sur la page de <span>Corentine Renuart</span></h1>
+                    <h2>Junior Front end developer</h2>
+                    <i class="fas fa-comment-dots"></i>
+                    <p>Motivée, créative et dynamique, je recherche un stage me permettant d’acquérir une première expérience dans le secteur de développement web.</p>
+                    {/* To upload the pdf cv */}
+                    <div className="uploadcv">
+                        <i class="fas fa-cloud-upload-alt"></i>
+                        <a href="./images/CV-Corentine-Renuart.pdf">Télécharger le CV en pdf</a>
                     </div>
                 </div>
             </div>
