ef2cbe8ef372013a5bf0e423408097880ad5c759
Corentine <renuart.corentine@gmail.com>
2021-12-05 00:18:58 +0100

    Adding the training component

---
+++ src/elements/Career/Training.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Training = () => {
+    return (
+        <div className="Training">
+            <h3>Formation</h3>
+            <div className="training-1">
+                <h4>ESA Saint-Luc Liège</h4>
+                <h5>Septembre 2015 - Juin 2020</h5>
+                <p>Master en design industriel obtenu le 2 juillet 2020</p>
+            </div>
+            <div className="training-2">
+                <h4>Centre IFAPME Liège-Huy-Verviers</h4>
+                <h5>Septembre 2020 - Juin 2021</h5>
+                <p>Cours du soir en design graphique.</p>
+            </div>
+            <div className="training-3">
+                <h4>ESD de la Región de Murcia</h4>
+                <h5>Septembre 2018 - Février 2019</h5>
+                <p>6 months Erasmus exchange at the Murcia School of Design</p>
+            </div>
+        </div>
+    );
+};
+
+export default Training;
--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from './Home';
 import Experiences from '../elements/Career/Experiences';
+import Training from '../elements/Career/Training';
 
 const Parcours = () => {
     return (
@@ -8,6 +9,7 @@ const Parcours = () => {
             <Navigation/>
             <h1>This is the parcours page</h1>
             <Experiences/>
+            <Training/>
         </div>
     );
 };
