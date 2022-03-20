d11e53a4ea5a593a2e23e0862ec89b98911be95e
Corentine <renuart.corentine@gmail.com>
2022-03-20 21:34:56 +0100

    Improvements training and experiences pages

--- src/elements/Career/Experiences.js
+++ src/elements/Career/Experiences.js
@@ -5,16 +5,18 @@ const Experiences = () => {
         <div className="Experiences">
             <h3>Expériences</h3>
             <div className="experiences">
-                <h4>UI/UX / Frontend (stage)</h4>
-                <h5>IOT Factory • Janvier 2022 - Avril 2022</h5>
+                <h4>Front-end dévelopeur (UI/UX)</h4>
+                <h5>IOT Factory, Bruxelles</h5>
+                <h6>Janvier - Avril 2022 (stage)</h6>
                 <ul>
-                    <li></li>
-                    <li></li>
+                    <li>Refonte entière de l'interface web de leur application : amélioration du design et de l'expérience utilisateur.</li>
+                    <li>Pug, Sass, Bootstrap, Angularjs</li>
                 </ul>
             </div>
             <div className="experiences">
                 <h4>Junior frontend developer</h4>
-                <h5>Becode.org • Juin 2021 - Janvier 2022</h5>
+                <h5>Becode.org, Liège</h5>
+                <h6>Juin 2021 - Janvier 2022</h6>
                 <ul>
                     <li>7 mois de formation intensive afin d'apprendre le développement web.</li>
                     <li>HTML5, CSS3, SASS, Bootstrap, Javascript, Reactjs, Wordpress, Php, Git, Npm.</li>
@@ -22,8 +24,9 @@ const Experiences = () => {
                 </ul>
             </div>
             <div className="experiences">
-                <h4>Design industriel (stage)</h4>
-                <h5>Aazio (Stratégie Agency) • Novembre 2019 - Janvier 2020</h5>
+                <h4>Designer industriel</h4>
+                <h5>Aazio (Stratégie Agency), Oupeye</h5>
+                <h6>Novembre 2019 - Janvier 2020 (stage)</h6>
                 <ul>
                     <li>Création d'emballages et d'objets publicitaires pour différentes marques telles qu'ING, Léonidas, Galler, Lyreco et Tilman.</li>
                     <li>Recherches / améliorations de concepts, modélisations / rendus 3D, plans industriels.</li>
--- src/elements/Career/Training.js
+++ src/elements/Career/Training.js
@@ -7,18 +7,19 @@ const Training = () => {
             <div className="training">
                 <h4>Formation en design graphique</h4>
                 <h5>Centre IFAPME Liège-Huy-Verviers</h5>
-                <p>Septembre 2020 - Juin 2021</p>
+                <h6>Septembre 2020 - Juin 2021</h6>
             </div>
             <div className="training">
                 <h4>Master en design industriel</h4>
                 <h5>ESA Saint-Luc Liège</h5>
-                <p>Septembre 2015 - Juin 2020</p>
+                <h6>Septembre 2015 - Juin 2020</h6>
                 <p>Diplôme obtenu le 2 juillet 2020</p>
             </div>
             <div className="training">
                 <h4>Echange Erasmus</h4>
                 <h5>Escuela Superior de Diseño de la Región de Murcia</h5>
-                <p>Septembre 2018 - Février 2019</p>
+                <h6>Septembre 2018 - Février 2019</h6>
+                <p>Diseño de producto</p>
             </div>
         </div>
     );
