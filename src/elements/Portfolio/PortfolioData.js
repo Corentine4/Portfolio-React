ad26ac91781ab6ef6cbcfd392ac5ebc5d0d444bc
Corentine <renuart.corentine@gmail.com>
2022-05-12 14:53:52 +0200

    Add pictures

--- src/elements/Portfolio/PortfolioData.js
+++ src/elements/Portfolio/PortfolioData.js
@@ -7,7 +7,7 @@ export const PortfolioData = [
         category: "Webdesign",
         description: "Refonte de l'application Web d'IOT Factory, une entreprise bruxelloise spécialisée dans l'internet des objets. Comme il n'y a pas de webdesigner dans l'équipe, j'ai été chargée de l'amélioration du design et surtout de l'expérience utilisateur. J'ai ensuite codé les solutions apportées en Pug, Sass, Bootstrap et Angularjs. A titre de comparaison, la troisième photo est un screenshot de l'interface avant modifications. ",
         picture: "./images/IOTFactory.png",
-        picturemodal: ["./images/IOTFactory2.png", "./images/IOTFactory3.png", "./images/IOTFactory4.png"] 
+        picturemodal: ["./images/IOTFactory2.png", "./images/IOTFactory3.png", "./images/IOTFactory4.png", "./images/IOTFactory5.png"] 
     },
     {
         id: 2,
@@ -16,7 +16,7 @@ export const PortfolioData = [
         date: "2021",
         category: "Webdesign",
         lien: "https://github.com/Corentine4/Productivity-application",
-        description: "Création d'un site web possèdant une Todo list et un Pomodoro afin d'augmenter sa productivité. Objectif : apprendre Reactjs. Projet toujours en cours : le style du Pomodoro doit être ajouté.",
+        description: "Création d'un site web possèdant une Todo list et un Pomodoro afin d'augmenter sa productivité. Objectif : apprendre Reactjs. Projet toujours en cours : le style du Pomodoro doit encore être ajouté.",
         picture: "./images/Productivityapplication.png",
         picturemodal: ["./images/Productivityapplicationmodal.png"]
     },
@@ -28,6 +28,6 @@ export const PortfolioData = [
         category: "Webdesign",
         description: "Durant ma formation chez Becode.org, j'ai été chargée de refaire le design du site « http://lamaisondc.be » sur Adobe XD en vue de créer une nouvelle eboutique. La création de celle-ci est actuellement en cours. Il s'agit d'un projet MERN codé par Eva Mélinand, Iris Renauld (back-end) et moi (front-end).",
         picture: "./images/DCMaison.png",
-        picturemodal: ["./images/MaisonDC2.png"]
+        picturemodal: ["./images/MaisonDC2.png", "./images/MaisonDC3.png"]
     }
 ]
